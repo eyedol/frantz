383edb1e1603c5ba7a9c07ac5687dcfc4f03ff25
Oliver Gilliland <omgilliland@gmail.com>
2017-12-02 15:22:48 +0100

    Adding data models

--- backend/fullstackpo/api/models.py
+++ backend/fullstackpo/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Bol(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    buyer = models.CharField(max_length=200)
+    vendor = models.CharField(max_length=200)
+
+    def __str__(self):
+    	return vendor
+
+class Case(models.Model):
+	bol = models.ForeignKey(Bol, on_delete=models.CASCADE)
+	itemName = models.CharField(max_length=200)
+	contract = models.CharField(max_length=200)
+	barcode = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.itemName
