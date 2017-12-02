decef7b91abc16a112f4fc3bb71fea258bb872f8
Walter Rothenberger <wr@hellofresh.com>
2017-12-02 16:19:02 +0100

    added auth

--- backend/fullstackpo/api/resources.py
+++ backend/fullstackpo/api/resources.py
@@ -1,7 +1,10 @@
 from tastypie.resources import ModelResource
 from api.models import Bol
+from tastypie.authorization import Authorization
+
 
 class BolResource(ModelResource):
 	class Meta:
 		queryset = Bol.objects.all()
 		resource_name = 'bol'
+		authorization = Authorization()
