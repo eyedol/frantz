603f269e2bb35bc30de4fddcbb92347e8f884031
Walter Rothenberger <wr@hellofresh.com>
2017-12-02 15:59:21 +0100

    NO TWO MODELS DAMN

--- backend/fullstackpo/api/models.py
+++ backend/fullstackpo/api/models.py
@@ -6,15 +6,16 @@ class Bol(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     buyer = models.CharField(max_length=200)
     vendor = models.CharField(max_length=200)
+    cases = models.TextField() #screw this, we'll serialize JSON
 
     def __str__(self):
     	return self.vendor
 
-class Case(models.Model):
-	bol = models.ForeignKey(Bol, on_delete=models.CASCADE)
-	itemName = models.CharField(max_length=200)
-	contract = models.CharField(max_length=200)
-	barcode = models.CharField(max_length=200)
+# class Case(models.Model):
+# 	bol = models.ForeignKey(Bol, on_delete=models.CASCADE)
+# 	itemName = models.CharField(max_length=200)
+# 	contract = models.CharField(max_length=200)
+# 	barcode = models.CharField(max_length=200)
 
-	def __str__(self):
-		return self.itemName
+# 	def __str__(self):
+# 		return self.itemName
